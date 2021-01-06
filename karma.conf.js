5a358572ea75e0edeb5f7a728715f408d86c752d
Sakkie van Zyl <sakkie89@gmail.com>
2021-01-06 20:27:43 +0200

    adding docker files and ci cd

--- karma.conf.js
+++ karma.conf.js
@@ -11,18 +11,39 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ioco-angular-framework'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
-    reporters: ['progress', 'kjhtml'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './analytics/coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'cobertura' }
+      ],
+      file: 'code-coverage.xml'
+    },
+     // the default configuration
+     junitReporter: {
+      outputDir: 'analytics/junit', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'report.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      useBrowserName: false, // add browser name to report and classes names
+    },
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,

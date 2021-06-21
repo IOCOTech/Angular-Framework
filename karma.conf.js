4bd5bc712f9a59777370c7737c4a2ee71e3c4f10
Sakkie van Zyl <sakkie.vanzyl@eoh.co.za>
2021-06-21 12:30:58 +0200

    Fixing project naming

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Angular-Framework'),
+      dir: require('path').join(__dirname, './coverage/ioco-angular-framework'),
       subdir: '.',
       reporters: [
         { type: 'html' },

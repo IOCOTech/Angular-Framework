2d6b7288a2f831eaba8fd63a63e7334ca54545f9
Sakkie van Zyl <sakkie.vanzyl@eoh.co.za>
2022-04-20 07:31:38 +0200

    updating msal config

--- src/app/modules/microsoft-authentication-library/microsoft-authentication-library.module.ts
+++ src/app/modules/microsoft-authentication-library/microsoft-authentication-library.module.ts
@@ -1,64 +1,28 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { MsalBroadcastService, MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent, MsalService } from '@azure/msal-angular';
-import { PublicClientApplication } from '@azure/msal-browser/dist/app/PublicClientApplication';
-import { BrowserCacheLocation, InteractionType } from '@azure/msal-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
-
-
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { MsalBroadcastService, MsalInterceptor, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG } from '@azure/msal-angular';
+import { ServiceConfig } from 'src/app/services/config.service/config.service';
+import { MSALGuardConfigFactory, MSALInstanceFactory, MSALInterceptorConfigFactory } from 'src/environments/msal/msal.config';
 @NgModule({
   declarations: [],
   imports: [
-    MsalModule.forRoot(new PublicClientApplication({ // MSAL Configuration
-      auth: {
-        clientId: '4756cca9-9d36-4e3a-a88a-ff1a8624a95b',
-        authority: 'https://aautologin.b2clogin.com/fc214cde-4549-4870-822f-4ecfdcf8b211/B2C_1_Signup_Signin/',
-        navigateToLoginRequestUrl: true,
-        knownAuthorities: ['https://aautologin.b2clogin.com'],
-        redirectUri: 'http://localhost:4200/oid-redirect'
-      },
-      cache: {
-        cacheLocation: BrowserCacheLocation.LocalStorage,
-        storeAuthStateInCookie: true, // set to true for IE 11
-      },
-      system: {
-        loggerOptions: {
-          loggerCallback: () => { },
-          piiLoggingEnabled: false
-        }
-      }
-    }), {
-      interactionType: InteractionType.Redirect, // MSAL Guard Configuration
-    }, {
-      interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
-      protectedResourceMap: new Map([
-        ['https://graph.microsoft.com/v1.0/me', ['user.read']]
-      ])
-    })
+    MsalModule
   ]
 })
 export class MicrosoftAuthenticationLibraryModule {
-
-
+  
   static forRoot(): ModuleWithProviders<MicrosoftAuthenticationLibraryModule> {
     return {
       ngModule: MicrosoftAuthenticationLibraryModule,
       providers: [
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: MsalInterceptor,
-          multi: true
-        },
+        { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true },
+        { provide: MSAL_INSTANCE, useFactory: MSALInstanceFactory, deps: [ServiceConfig] },
+        { provide: MSAL_GUARD_CONFIG, useFactory: MSALGuardConfigFactory },
+        { provide: MSAL_INTERCEPTOR_CONFIG, useFactory: MSALInterceptorConfigFactory },
+        // { provide: AbstractRouteGuard, useFactory: FactoryRouteGuard, deps: [MSAL_GUARD_CONFIG, MsalBroadcastService, MsalService, Location, Router]  },
         MsalService,
-        MsalGuard,
-        MsalBroadcastService
+        MsalBroadcastService,
       ]
     };
   }
-  // static forRoot(): any {
-  //   return {
-  //     NgModule: MicrosoftAuthenticationLibraryModule,
-
-  //    };
-  // }
 }
--- src/environments/msal/msal.config.ts
+++ src/environments/msal/msal.config.ts
@@ -1,42 +1,52 @@
-import { Configuration } from 'msal';
-import { MsalAngularConfiguration } from '@azure/msal-angular';
-import { AppSettings } from '../app-settings/app-settings';
+import { MsalGuardConfiguration, MsalInterceptorConfiguration } from '@azure/msal-angular';
+import { BrowserCacheLocation, InteractionType, IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 import { ServiceConfig } from 'src/app/services/config.service/config.service';
 
 export const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
-export const protectedResourceMap: [string, string[]][] = [
-    ['https://graph.microsoft.com/v1.0/me', ['user.read']],
-    ['https://localhost:44386', ['https://{{app-name}}.onmicrosoft.com/{{app-name}}-api/user_impersonation']]
-];
-
-export function MSALConfigFactory(serviceConfig: ServiceConfig): Configuration {
-    return {
+// export const protectedResourceMap: [string, string[]][] = [
+//     ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+// ];
+// TODO: Update knownAuthorities to read from config
+export function MSALInstanceFactory(serviceConfig: ServiceConfig): IPublicClientApplication {
+    return new PublicClientApplication({
         auth: {
             clientId: serviceConfig.authentication.clientId,
             authority: serviceConfig.authentication.authority,
-            validateAuthority: serviceConfig.authentication.validateAuthority,
+            navigateToLoginRequestUrl: serviceConfig.authentication.navigateToLoginRequestUrl,
+            knownAuthorities: ['https://arenalogin.b2clogin.com', 'http://localhost:4200'],
             redirectUri: serviceConfig.authentication.redirectUri,
-            postLogoutRedirectUri: serviceConfig.authentication.postLogoutRedirectUri,
-            navigateToLoginRequestUrl: serviceConfig.authentication.navigateToLoginRequestUrl
-        },
-        cache: {
-            cacheLocation: 'localStorage',
-            storeAuthStateInCookie: isIE, // set to true for IE 11
-        }
-    };
+            postLogoutRedirectUri: serviceConfig.authentication.postLogoutRedirectUri
+          },
+          cache: {
+            cacheLocation: BrowserCacheLocation.LocalStorage,
+            storeAuthStateInCookie: true, // set to true for IE 11
+          },
+          system: {
+            loggerOptions: {
+              loggerCallback: () => { },
+              piiLoggingEnabled: false
+            }
+          }
+    });
 }
 
-export function MSALAngularConfigFactory(): MsalAngularConfiguration {
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+    const protectedResourceMap = new Map<string, Array<string>>();
+    protectedResourceMap.set("https://graph.microsoft.com/v1.0/me", ["user.read"]);
+  
     return {
-        popUp: false,
-        consentScopes: [
-            'openid',
-            'profile',
-            'https://{{app-name}}.onmicrosoft.com/{{app-name}}-api/user_impersonation'
-        ],
-        unprotectedResources: [AppSettings.configFileLocation],
-        protectedResourceMap,
-        extraQueryParameters: {}
+      interactionType: InteractionType.Redirect,
+      protectedResourceMap
     };
-}
+  }
+  
+  export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+    return { 
+      interactionType: InteractionType.Redirect,
+      authRequest: { 
+        // scopes: ['user.read']
+      },
+      loginFailedRoute: "home"
+    };
+  }
